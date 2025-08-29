74f23acb2f5f47d811f2e442f38b34ee153d8465
Jonayet-06 <jonayethossain3484@gmail.com>
2025-08-29 06:18:30 +0600

    added responsive for mobile device

---
+++ main.js
@@ -0,0 +1,27 @@
+document.getElementById('heart-1').addEventListener('click', function(){
+    console.log('Connected Successfully-1');
+})
+document.getElementById('heart-2').addEventListener('click', function(){
+    console.log('Connected Successfully-2');
+})
+document.getElementById('heart-3').addEventListener('click', function(){
+    console.log('Connected Successfully-3');
+})
+document.getElementById('heart-4').addEventListener('click', function(){
+    console.log('Connected Successfully-4');
+})
+document.getElementById('heart-5').addEventListener('click', function(){
+    console.log('Connected Successfully-5');
+})
+document.getElementById('heart-6').addEventListener('click', function(){
+    console.log('Connected Successfully-6');
+})
+document.getElementById('heart-7').addEventListener('click', function(){
+    console.log('Connected Successfully-7');
+})
+document.getElementById('heart-8').addEventListener('click', function(){
+    console.log('Connected Successfully-8');
+})
+document.getElementById('heart-9').addEventListener('click', function(){
+    console.log('Connected Successfully-9');
+})
