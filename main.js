2d34e147419cc191e3ac42b4c41353d9a9b60361
Jonayet-06 <jonayethossain3484@gmail.com>
2025-08-29 14:32:13 +0600

    Added heart count

--- main.js
+++ main.js
@@ -1,27 +1,14 @@
-document.getElementById('heart-1').addEventListener('click', function(){
-    console.log('Connected Successfully-1');
-})
-document.getElementById('heart-2').addEventListener('click', function(){
-    console.log('Connected Successfully-2');
-})
-document.getElementById('heart-3').addEventListener('click', function(){
-    console.log('Connected Successfully-3');
-})
-document.getElementById('heart-4').addEventListener('click', function(){
-    console.log('Connected Successfully-4');
-})
-document.getElementById('heart-5').addEventListener('click', function(){
-    console.log('Connected Successfully-5');
-})
-document.getElementById('heart-6').addEventListener('click', function(){
-    console.log('Connected Successfully-6');
-})
-document.getElementById('heart-7').addEventListener('click', function(){
-    console.log('Connected Successfully-7');
-})
-document.getElementById('heart-8').addEventListener('click', function(){
-    console.log('Connected Successfully-8');
-})
-document.getElementById('heart-9').addEventListener('click', function(){
-    console.log('Connected Successfully-9');
-})
+// for adding heart
+const hearts = document.getElementsByClassName('heart');
+// console.log(hearts);
+
+for(const heart of hearts){
+    heart.addEventListener('click', function(){
+        const totalHeart = document.getElementById('mainHeart').innerText;
+         const mainHeart = parseInt(totalHeart);
+
+         // current heart
+         const currentHeart = mainHeart + 1;
+         document.getElementById('mainHeart').innerText = currentHeart;
+    })
+}
